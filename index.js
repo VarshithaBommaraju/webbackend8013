b433fd0e34461ed2688ae47eb1197a36a4858b38
VarshithaBommaraju <166783485+VarshithaBommaraju@users.noreply.github.com>
2024-12-02 13:49:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -54,19 +54,6 @@ const server = http.createServer(async (req, res) => {
       res.end(data);
     });
   }
-    else if (req.url === '/about') {
-        
-        fs.readFile(
-            path.join(__dirname, 'public', 'about.html'),
-                    (err, content) => {
-                                    
-                                    if (err) throw err;
-                                    res.setHeader("Access-Control-Allow-Origin", "*")
-                                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                                    res.end(content);
-                        }
-              );
-     }
     else if (req.url==='/api')
     {
     res.setHeader('Access-Control-Allow-Origin', '*');
